6417c4ab6a08466df5034596760e4e6636b7f46a
Thanh Tran <python0429@gmail.com>
2016-10-21 14:28:58 -0400

    API change

--- threex.lasercooked.js
+++ threex.lasercooked.js
@@ -1,67 +1,63 @@
-var THREEx = THREEx || {}
+'use strict';
 
-THREEx.LaserCooked	= function(laserBeam){
+var THREEx = THREEx || {};
+
+THREEx.LaserCooked = function(laserBeam,texture,scene){
 	// for update loop
-	var updateFcts	= []
+	var updateFcts = [];
 	this.update	= function(delta, now){
 		updateFcts.forEach(function(updateFct){
 			updateFct(delta, now)	
-		})
-	}
+		});
+	};
 	
-	var object3d	= laserBeam.object3d
+	var object3d = laserBeam.object3d;
 
 	// build THREE.Sprite for impact
-	var textureUrl	= THREEx.LaserCooked.baseURL+'images/blue_particle.jpg';
-	var texture	= THREE.ImageUtils.loadTexture(textureUrl)	
-	var material	= new THREE.SpriteMaterial({
-		map			: texture,
-		blending		: THREE.AdditiveBlending,
-		useScreenCoordinates	: false,
-	})
-	var sprite	= new THREE.Sprite(material)
-	sprite.scale.set(1, 1, 1).multiplyScalar(2)
-	sprite.position.x	= 1
-	object3d.add(sprite)
+	var material = new THREE.SpriteMaterial({
+		map: texture,
+		blending: THREE.AdditiveBlending,
+		//useScreenCoordinates	: false,
+	});
+	var sprite = new THREE.Sprite(material);
+	sprite.scale.set(2,2,2);
+	sprite.position.x = 1;
+	object3d.add(sprite);
 
 	// add a point light
-	var light	= new THREE.PointLight( 0x4444ff, 10 );
-	light.intensity	= 2
-	light.distance	= 4
-	light.position.x= -0.05
-	this.light	= light
-	sprite.add(light)
+	var light = new THREE.PointLight( 0x4444ff, 10 );
+	light.intensity	= 2;
+	light.distance = 4;
+	light.position.x = -0.05;
+	this.light = light;
+	sprite.add(light);
 
 	// to exports last intersects
 	this.lastIntersects	= []
 
-	var raycaster	= new THREE.Raycaster()
-	// TODO assume object3d.position are worldPosition. works IFF attached to scene
-	raycaster.ray.origin.copy(object3d.position)
+	var raycaster = new THREE.Raycaster()
+	raycaster.ray.origin.copy(object3d.position);
 	updateFcts.push(function(delta, now){
 		// get laserBeam matrixWorld
 		object3d.updateMatrixWorld();
-		var matrixWorld	= object3d.matrixWorld.clone()
+		var matrixWorld	= object3d.matrixWorld.clone();
 		// set the origin
-		raycaster.ray.origin.getPositionFromMatrix(matrixWorld)
+		raycaster.ray.origin.setFromMatrixPosition(matrixWorld);
 		// keep only the roation
-		matrixWorld.setPosition(new THREE.Vector3(0,0,0))		
+		matrixWorld.setPosition(new THREE.Vector3(0,0,0));		
 		// set the direction
 		raycaster.ray.direction.set(1,0,0)
-			.applyMatrix4( matrixWorld )
-			.normalize()
+			.applyMatrix4(matrixWorld)
+			.normalize();
 
-		var intersects		= raycaster.intersectObjects( scene.children );
-		if( intersects.length > 0 ){
-			var position	= intersects[0].point
-			var distance	= position.distanceTo(object3d.position)
-			object3d.scale.x	= distance
-		}else{
-			object3d.scale.x	= 10			
+		var intersects = raycaster.intersectObjects(scene.children);
+		if(intersects.length > 0) {
+			var position = intersects[0].point;
+			var distance = position.distanceTo(object3d.position);
+			object3d.scale.x = distance;
+		} else{
+			object3d.scale.x = 10;		
 		}
-		// backup last intersects
-		this.lastIntersects	= intersects
+		this.lastIntersects	= intersects;
 	}.bind(this));
-}
-
-THREEx.LaserCooked.baseURL	= '../'
+};
